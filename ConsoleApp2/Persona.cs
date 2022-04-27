2c3d73963430c8d1134ff3792a0e1a01295fb805
DAR-LAB-MA\UCSE <a>
2022-04-27 19:58:48 -0300

    Commit primer ejercicio

---
+++ ConsoleApp2/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Persona
+    {
+        public EventHandler<ActualizacionEventArgs> ActualizacionNombreCompletado;
+        private string Nombre { get; set; }
+        public string ActualizarNombre (string Nuevonombre)
+        {
+            Nombre = Nuevonombre;
+            if (this.ActualizacionNombreCompletado != null)
+                this.ActualizacionNombreCompletado(this, new ActualizacionEventArgs() { Nombre = Nombre });
+            return Nombre;
+        }
+
+
+}
+}
