a1fc333a67b837afb3d2c39db86babeddf6e4d34
Mati <mati.delbono00@gmail.com>
2022-04-29 17:20:49 -0300

    Commit última parte con dudas

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,6 +9,9 @@
     }
     static void ActualizacionNombreCompletadoHandler(object? sender, ActualizacionEventArgs e)
     {
-        Console.Writeline("termino la actualizacion");
+        Console.Writeline("termino la actualizacion de" + Nombre, "a" + NombreNuevo);
     }
+    // duda 1 : Cómo configuro al suscriptor si este está en una clase distinta de la que tiene el evento asociado?
+    // duda 2: Cómo se pasa el método ActualizarNombre como parámetro a través del delegado ActualizacionNombreCompletado?
 }
+
